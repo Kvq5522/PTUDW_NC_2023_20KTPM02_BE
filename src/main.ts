89c55f5437b7c058e3c41476a6101814f61a0597
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 23:12:56 +0700

    try no cors

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,PATCH,DELETE,POST,PUT',
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'Access-Control-Allow-Origin',
-    ],
-    preflightContinue: false,
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: false });
 
   app.useGlobalPipes(
     new ValidationPipe({
