9bc80e79c3a3df7398b9168e39c9539c56fc12bb
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 22:06:00 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3000',
+      'https://qvk-hhdh-ptudw-nc-2023-20ktpm02.vercel.app/',
+    ],
     methods: 'GET,PATCH,DELETE,POST,PUT',
     allowedHeaders:
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    credentials: true,
     preflightContinue: false,
+    credentials: true,
   });
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.headers.origin && req.method === 'OPTIONS') {
+      return res.status(200).send('handled');
+    } else {
+      return next();
+    }
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(1507);
 }
 bootstrap();
