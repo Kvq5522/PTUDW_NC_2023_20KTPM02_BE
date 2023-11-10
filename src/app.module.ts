cf9bf7584e5366d73769763126cf58b9f0b9ce03
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 19:06:44 +0700

    test for vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, AuthModule, ConfigModule.forRoot(), PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello() {
+    return {
+      statusCode: 200,
+      message: 'Hello World!',
+      metadata: {},
+    };
+  }
+}
