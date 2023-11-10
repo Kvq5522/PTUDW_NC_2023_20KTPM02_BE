ed4d206e4684248b0f506df9bb6b02c9386152c3
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 21:19:49 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3000',
+      'https://qvk-hhdh-ptudw-nc-2023-20ktpm02.vercel.app',
+    ],
     methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
     allowedHeaders:
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
     credentials: true,
+    preflightContinue: false,
   });
   app.useGlobalPipes(
     new ValidationPipe({
