190e16753ac29291dcb195cc10b1228d96075009
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 22:51:08 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,17 +11,10 @@ async function bootstrap() {
       'https://qvk-hhdh-ptudw-nc-2023-20ktpm02.vercel.app/',
     ],
     methods: 'GET,PATCH,DELETE,POST,PUT',
-    allowedHeaders: '*',
+    allowedHeaders:
+      'Content-Type, Accept, Authorization, Access-Control-Allow-Origin',
     preflightContinue: false,
     credentials: true,
-  });
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.headers.origin && req.method === 'OPTIONS') {
-      return res.status(200).send('handled');
-    } else {
-      return next();
-    }
   });
 
   app.useGlobalPipes(
