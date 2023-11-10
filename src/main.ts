50e3e540537094449e337cbbe680409cba478a32
unknown <vinhkhangquach2002@gmail.com>
2023-11-10 23:22:39 +0700

    try simple cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://qvk-hhdh-ptudw-nc-2023-20ktpm02.vercel.app',
+    ],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
